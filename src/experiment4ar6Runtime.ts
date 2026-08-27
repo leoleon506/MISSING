db7f3d3d6438a18e376057534885838686c3eb13
Leonardo León <leo_leon506@hotmail.com>
2026-08-26 21:56:24 -0600

    Add R6 runtime exports

---
+++ src/experiment4ar6Runtime.ts
@@ -0,0 +1,3 @@
+export {extractEvidenceFrontierR5} from "./experiment4ar5Core.js";
+export {specSummaryR6} from "./experiment4ar6Core.js";
+export {auditProviderBlindSource} from "./experiment4ar3Core.js";
