59b9f2247dc910ede09e8753b3f57997e27e9074
Leonardo León <leo_leon506@hotmail.com>
2026-08-27 09:52:04 -0600

    Add P1 runtime exports

---
+++ src/experiment4ap1Runtime.ts
@@ -0,0 +1,3 @@
+export {extractEvidenceFrontierR5 as extractFrontierLinks} from "./experiment4ar5Core.js";
+export {specSummaryR6R as specSummaryP1} from "./experiment4ar6rEvidence.js";
+export {auditProviderBlindSource} from "./experiment4ar3Core.js";
