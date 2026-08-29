6b2d8f2c3449d262a1b86db8eff422ab5513f154
Leonardo León <leo_leon506@hotmail.com>
2026-08-29 06:30:33 -0600

    Reuse strict 5B2 semantic contract in Experiment 5B5

---
+++ src/experiment5b5Contract.ts
@@ -0,0 +1,1 @@
+export {validate5b2 as validate5b5,compile5b2 as compile5b5,project5b2 as project5b5,validateProjected5b2 as validateProjected5b5,scope5b2 as scope5b5} from "./experiment5b2Contract.js";
