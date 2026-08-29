c8f527eea247f9095af30ed53dbc26dfe5f1b08c
Leonardo León <leo_leon506@hotmail.com>
2026-08-29 00:54:41 -0600

    Reuse unchanged 5B2 semantic contract in Experiment 5B3

---
+++ src/experiment5b3Contract.ts
@@ -0,0 +1,1 @@
+export {validate5b2 as validate5b3,compile5b2 as compile5b3,project5b2 as project5b3,validateProjected5b2 as validateProjected5b3,scope5b2 as scope5b3} from "./experiment5b2Contract.js";
